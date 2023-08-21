8cdb30c501e08a67a206c6c74f9872ef3148fac6
nehal <nehal-prasad@outlook.com>
2023-08-21 13:11:10 +0530

    all files

---
+++ src/Screens/UserDashboard.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UserDashboard = () => {
+  return (
+    <View>
+      <Text>UserDashboard</Text>
+    </View>
+  )
+}
+
+export default UserDashboard
+
+const styles = StyleSheet.create({})
